0dff76ed133162b3d95836f0d29136a1a78dac9c
ygohappy123 <hagiahuy21052003@gmail.com>
2024-12-16 23:07:05 +0700

    set up modules and blynk

--- hardware/arduino_uno/arduino_uno.ino
+++ hardware/arduino_uno/arduino_uno.ino
@@ -1,40 +1,57 @@
+#define DISTANCE_TRIG_PIN 2
+#define DISTANCE_ECHO_PIN 3
+#define DHT_PIN 4
+#define TEMPERATURE_PIN 8
+#define TDS_PIN A1
+#define VREF 5.0
+#define BAUD_RATE 115200
+#define LOOP_DELAY 1000
+
+#include <DHT.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <LiquidCrystal_I2C.h>
 #include <ArduinoJson.h>
 
-#define DISTANCE_TRIG_PIN 2
-#define DISTANCE_ECHO_PIN 3
-#define TEMPERATURE_PIN 8
-#define BAUD_RATE 115200
-#define LOOP_DELAY 1000
-
+DHT dht(DHT_PIN, DHT11);
 OneWire oneWire(TEMPERATURE_PIN);
 DallasTemperature temperatureSensor(&oneWire);
 LiquidCrystal_I2C lcdDisplay(0x27, 20, 4);
 
+String jsonInput;
 float waterTankHeight = 0;
 float waterLevel = 0;
 float temperature = 0;
 float tdsLevel = 0;
 float humidity = 0;
 
+int pumpIn = 0;
+int pumpOut = 0;
+int cooler = 0;
+int heater = 0;
+int tdsPlants = 0;
+int tdsFishes = 0;
+int mistSprayer = 0;
+int ventilation = 0;
+
 void setup () {
     Serial.begin(BAUD_RATE);
     
+    dht.begin();
     lcdDisplay.init();
     lcdDisplay.backlight();
 
     pinMode(DISTANCE_TRIG_PIN, OUTPUT);
     pinMode(DISTANCE_ECHO_PIN, INPUT);
+    pinMode(TDS_PIN, INPUT);
 }
 
 void loop() {
-    // waterLevel = waterTankHeight - measureDistance();
-    // temperature = getWaterTemperature();
-    // tdsLevel = 0;
-
-    String jsonInput;
+    waterLevel = waterTankHeight - getDistanceToWaterSurface();
+    temperature = getWaterTemperature();
+    tdsLevel = getWaterTdsValue();
+    humidity = getEnvironmentHumidity();
+    printJsonObjectToSerial (waterLevel, temperature, tdsLevel, humidity);
 
     while (Serial.available()) {
         jsonInput = Serial.readStringUntil('\n');
@@ -43,15 +60,6 @@ void loop() {
 
     if (jsonInput.length() > 0) {
         parseJsonObjectFromSerial(jsonInput);
-
-        Serial.print("Water level: ");
-        Serial.println(waterLevel);
-        Serial.print("Temperature: ");
-        Serial.println(temperature);
-        Serial.print("Humidity: ");
-        Serial.println(humidity);
-        Serial.print("TDS level: ");
-        Serial.println(tdsLevel);
     }
 
     delay(LOOP_DELAY);
@@ -65,7 +73,16 @@ float getWaterTemperature () {
     return celsius;
 }
 
-float measureDistance () {
+float getWaterTdsValue() {
+    float rawEc = analogRead(TDS_PIN) * VREF / 1023.0;
+    float temperatureCoefficient = 1.0 + 0.02 * (temperature - 25.0);
+    float volatge = rawEc / temperatureCoefficient;
+    float totalDissolvedSolids = (133.42 * volatge * volatge * volatge - 255.86 * volatge * volatge + 857.39 * volatge) * 0.5;
+
+    return totalDissolvedSolids;
+}
+
+float getDistanceToWaterSurface () {
     digitalWrite(DISTANCE_TRIG_PIN, LOW);
     delayMicroseconds(2);
     digitalWrite(DISTANCE_TRIG_PIN, HIGH);
@@ -76,6 +93,12 @@ float measureDistance () {
     float distance = (duration * 0.034) / 2;
 
     return distance;
+}
+
+float getEnvironmentHumidity () {
+    float envHumidity = dht.readHumidity();
+    
+    return envHumidity;
 }
 
 void displayMessageToLCD (int row, String message) {
@@ -108,10 +131,12 @@ void parseJsonObjectFromSerial (const String& jsonInput) {
 
     if (error) return;
 
-    if (doc.containsKey("waterLevel")) waterLevel = doc["waterLevel"];
-    if (doc.containsKey("temperature")) temperature = doc["temperature"];
-    if (doc.containsKey("humidity")) humidity = doc["humidity"];
-    if (doc.containsKey("tdsLevel")) tdsLevel = doc["tdsLevel"];
+    if (doc.containsKey("pumpIn")) pumpIn = doc["pumpIn"];
+    if (doc.containsKey("pumpOut")) pumpOut = doc["pumpOut"];
+    if (doc.containsKey("cooler")) cooler = doc["cooler"];
+    if (doc.containsKey("heater")) heater = doc["heater"];
+    if (doc.containsKey("tdsPlants")) tdsPlants = doc["tdsPlants"];
+    if (doc.containsKey("tdsFishes")) tdsFishes = doc["tdsFishes"];
+    if (doc.containsKey("mistSprayer")) mistSprayer = doc["mistSprayer"];
+    if (doc.containsKey("ventilation")) ventilation = doc["ventilation"];
 }
-
-
--- hardware/esp_8266/esp_8266.ino
+++ hardware/esp_8266/esp_8266.ino
@@ -1,31 +1,35 @@
-#include <ESP8266WiFi.h>
-#include <ArduinoJson.h>
-// #include <BlynkSimpleEsp8266.h>
-#include <Firebase_ESP_Client.h>
-#include "addons/TokenHelper.h"
-#include "addons/RTDBHelper.h"
-
 #define WIFI_SSID "PTIT.HCM_CanBo"
 #define WIFI_PASSWORD ""
 #define FIREBASE_DATABASE_URL "https://water-quality-management-ptit-default-rtdb.asia-southeast1.firebasedatabase.app/"
 #define FIREBASE_API_KEY "AIzaSyDJ-hOSSUSqD19g0xfG9XledGUfWOBE9ek"
-#define BLYNK_TEMPLATE_ID "xxx"
-#define BLYNK_TEMPLATE_NAME "xxx"
-#define BLYNK_AUTH_TOKEN "xxx"
+#define BLYNK_TEMPLATE_ID "TMPL6m1lE5i40"
+#define BLYNK_TEMPLATE_NAME "water quality management"
+#define BLYNK_AUTH_TOKEN "JmZkqgvAlVLzmv9qRxGgsoYXe4_-680B"
 #define BAUD_RATE 115200
 #define DATA_POSTING_INTERVAL 5000
+
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <BlynkSimpleEsp8266.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
 
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-bool isAutomatic = true;
+String jsonInput;
+bool isAutomatic = false;
 bool isUpdated = false;
-int device1 = -1;
-int device2 = -1;
-int device3 = -1;
-int device4 = -1;
-int device5 = -1;
+int pumpIn = 0;
+int pumpOut = 0;
+int cooler = 0;
+int heater = 0;
+int tdsPlants = 0;
+int tdsFishes = 0;
+int mistSprayer = 0;
+int ventilation = 0;
 
 unsigned long sendDataPrevMillis = 0;
 float waterLevel = 0;
@@ -40,11 +44,11 @@ void setup() {
     while (WiFi.status() != WL_CONNECTED) {
       delay(300);
     }
-    // Serial.println("Connected to WiFi!");
+    Serial.println("Connected to WiFi!");
 
-    // Blynk.config(BLYNK_AUTH_TOKEN);
-    // Blynk.connect();
-    // Serial.println("Connected to Blynk!");
+    Blynk.config(BLYNK_AUTH_TOKEN);
+    Blynk.connect();
+    Serial.println("Connected to Blynk!");
 
     config.database_url = FIREBASE_DATABASE_URL;
     config.api_key = FIREBASE_API_KEY;
@@ -53,12 +57,11 @@ void setup() {
     config.token_status_callback = tokenStatusCallback;
     Firebase.begin(&config, &auth);
     Firebase.reconnectWiFi(true);
-    // Serial.println("Connected to Firebase!");
+    Serial.println("Connected to Firebase!");
 }
 
 void loop() {
-    // Blynk.run();
-    String jsonInput;
+    Blynk.run();
 
     while (Serial.available()) {
         jsonInput = Serial.readStringUntil('\n');
@@ -79,20 +82,23 @@ void loop() {
     }
 
     if (isUpdated) {
-        printJsonObjectToSerial(device1, device2, device3, device4, device5);
+        printJsonObjectToSerial(pumpIn, pumpOut, cooler, heater, tdsPlants, tdsFishes, mistSprayer, ventilation);
         isUpdated = false;
     }
 }
 
-void printJsonObjectToSerial (int device1, int device2, int device3, int device4, int device5) {
+void printJsonObjectToSerial (int pumpIn, int pumpOut, int cooler, int heater, int tdsPlants, int tdsFishes, int mistSprayer, int ventilation) {
     StaticJsonDocument<200> doc;
     String jsonOutput;
 
-    doc["device_1"] = device1;
-    doc["device_2"] = device2;
-    doc["device_3"] = device3;
-    doc["device_4"] = device4;
-    doc["device_5"] = device5;
+    doc["pumpIn"] = pumpIn;
+    doc["pumpOut"] = pumpOut;
+    doc["cooler"] = cooler;
+    doc["heater"] = heater;
+    doc["tdsPlants"] = tdsPlants;
+    doc["tdsFishes"] = tdsFishes;
+    doc["mistSprayer"] = mistSprayer;
+    doc["ventilation"] = ventilation;
 
     serializeJson(doc, jsonOutput);
     Serial.println(jsonOutput);
@@ -112,10 +118,6 @@ void parseJsonObjectFromSerial (const String& jsonInput) {
     if (doc.containsKey("tdsLevel")) tdsLevel = doc["tdsLevel"];
 }
 
-// BLYNK_WRITE(V1) {
-//     int newValue = param.asInt();
-//     if (newValue != device1) {
-//         device1 = newValue;
-//         isUpdated = true;
-//     }
-// }
+BLYNK_WRITE(V0) {
+    isAutomatic = param.asInt();
+}
