1206ada5a693f4be00de353951dd8a477641e4e2
ygohappy123 <hagiahuy21052003@gmail.com>
2024-12-20 20:39:22 +0700

    finish arduino and esp code

--- hardware/arduino_uno/arduino_uno.ino
+++ hardware/arduino_uno/arduino_uno.ino
@@ -1,13 +1,20 @@
 #define DISTANCE_TRIG_PIN 2
 #define DISTANCE_ECHO_PIN 3
 #define DHT_PIN 4
+#define HEATER_PIN 5
+#define COOLER_PIN 6
+#define VENTILATION_PIN 7
 #define TEMPERATURE_PIN 8
+#define MIST_SPRAYER_PIN 9
+#define PUMP_IN_PIN 10
+#define PUMP_OUT_PIN 13
 #define TDS_PIN A1
 #define VREF 5.0
 #define BAUD_RATE 115200
 #define LOOP_DELAY 1000
 
 #include <DHT.h>
+#include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <LiquidCrystal_I2C.h>
@@ -44,18 +51,26 @@ void setup () {
     pinMode(DISTANCE_TRIG_PIN, OUTPUT);
     pinMode(DISTANCE_ECHO_PIN, INPUT);
     pinMode(TDS_PIN, INPUT);
+
+    pinMode(PUMP_IN_PIN, OUTPUT);
+    pinMode(PUMP_OUT_PIN, OUTPUT);
+    pinMode(COOLER_PIN, OUTPUT);
+    pinMode(HEATER_PIN, OUTPUT);
+    pinMode(VENTILATION_PIN, OUTPUT);
+    pinMode(MIST_SPRAYER_PIN, OUTPUT);
 }
 
 void loop() {
-    // waterLevel = waterTankHeight - getDistanceToWaterSurface();
-    // temperature = getWaterTemperature();
-    // tdsLevel = getWaterTdsValue();
-    // humidity = getEnvironmentHumidity();
-    waterLevel = 26.687;
-    temperature = 31.6875;
-    tdsLevel = 72;
-    humidity = 56.54631;
+    waterLevel = waterTankHeight - getDistanceToWaterSurface();
+    temperature = getWaterTemperature();
+    tdsLevel = getWaterTdsValue();
+    humidity = getEnvironmentHumidity();
+    // waterLevel = 26.687;
+    // temperature = 31.6875;
+    // tdsLevel = 72;
+    // humidity = 56.54631;
     printJsonObjectToSerial (waterLevel, temperature, tdsLevel, humidity);
+    displaySensorsMessage(waterLevel, temperature, tdsLevel, humidity);
 
     while (Serial.available()) {
         jsonInput = Serial.readStringUntil('\n');
@@ -64,6 +79,17 @@ void loop() {
 
     if (jsonInput.length() > 0) {
         parseJsonObjectFromSerial(jsonInput);
+
+        digitalWrite(PUMP_OUT_PIN, pumpOut == 1 ? LOW : HIGH);
+        digitalWrite(PUMP_IN_PIN, pumpIn == 1 ? HIGH : LOW);
+        digitalWrite(COOLER_PIN, cooler == 1 ? HIGH : LOW);
+        digitalWrite(HEATER_PIN, heater == 1 ? HIGH : LOW);
+        digitalWrite(VENTILATION_PIN, ventilation == 1 ? HIGH : LOW);
+        digitalWrite(MIST_SPRAYER_PIN, mistSprayer == 1 ? HIGH : LOW);
+
+        displayTdsMessage(tdsLevel);
+
+        jsonInput = "";
     }
 
     delay(LOOP_DELAY);
@@ -112,6 +138,44 @@ void displayMessageToLCD (int row, String message) {
     lcdDisplay.print("                    ");
     lcdDisplay.setCursor(0, row);
     lcdDisplay.print(message);
+}
+
+void displaySensorsMessage (float waterLevel, float temperature, float tdsLevel, float humidity) {
+    String line1 = "T";
+    line1 += (char)223;
+    line1 += ": " + String(temperature, 1) + "  TDS: " + String(tdsLevel, 1);
+
+    String line2 = "RH: " + String(roundf(humidity)) + "    WL: " + String(waterLevel, 2);
+
+    displayMessageToLCD(0, line1);
+    displayMessageToLCD(1, line2);
+}
+
+void displayTdsMessage (float tdsLevel) {
+    if (tdsPlants == 1 && tdsFishes == 1) {
+        displayMessageToLCD(2, "Ca Nuoc Ngot: Tot");
+        displayMessageToLCD(3, "Cay Thuy Sinh: Tot");
+    }
+
+    if (tdsPlants == 1 && tdsFishes == 0) {
+        displayMessageToLCD(2, "Ca Nuoc Ngot: Kem");
+        displayMessageToLCD(3, "Cay Thuy Sinh: Tot");
+    }
+
+    if (tdsPlants == 0 && tdsFishes == 1) {
+        displayMessageToLCD(2, "Ca Nuoc Ngot: Tot");
+        displayMessageToLCD(3, "Cay Thuy Sinh: Kem");
+    }
+
+    if (tdsPlants == 0 && tdsFishes == 0) {
+        displayMessageToLCD(2, "Nuoc Khong Phu Hop");
+
+        if (tdsLevel <= 100) {
+            displayMessageToLCD(3, "Can Them Dinh Duong");
+        } else {
+            displayMessageToLCD(4, "Vui Long Thay Nuoc");
+        }
+    }
 }
 
 void printJsonObjectToSerial (float waterLevel, float temperature, float tdsLevel, float humidity) {
--- hardware/esp_8266/esp_8266.ino
+++ hardware/esp_8266/esp_8266.ino
@@ -46,15 +46,18 @@ float humidity = 0;
 void setup() {
     Serial.begin(BAUD_RATE);
 
+    pinMode(AUTO_BUTTON_PIN, INPUT);
+    pinMode(AUTO_LED_PIN, OUTPUT);
+
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     while (WiFi.status() != WL_CONNECTED) {
       delay(300);
     }
-    // Serial.println("Connected to WiFi!");
+    Serial.println("Connected to WiFi!");
 
     Blynk.config(BLYNK_AUTH_TOKEN);
     Blynk.connect();
-    // Serial.println("Connected to Blynk!");
+    Serial.println("Connected to Blynk!");
 
     config.database_url = FIREBASE_DATABASE_URL;
     config.api_key = FIREBASE_API_KEY;
@@ -63,9 +66,17 @@ void setup() {
     config.token_status_callback = tokenStatusCallback;
     Firebase.begin(&config, &auth);
     Firebase.reconnectWiFi(true);
-    // Serial.println("Connected to Firebase!");
+    Serial.println("Connected to Firebase!");
 
     Blynk.syncVirtual(V0);
+    Blynk.syncVirtual(V1);
+    Blynk.syncVirtual(V2);
+    Blynk.syncVirtual(V3);
+    Blynk.syncVirtual(V4);
+    Blynk.syncVirtual(V5);
+    Blynk.syncVirtual(V6);
+    Blynk.syncVirtual(V7);
+    Blynk.syncVirtual(V8);
 }
 
 void loop() {
@@ -89,6 +100,8 @@ void loop() {
             Firebase.RTDB.setFloat(&fbdo, "Sensors/tds_level", tdsLevel);
             Firebase.RTDB.setFloat(&fbdo, "Sensors/env_humidity", humidity);
         }
+
+        jsonInput = "";
     }
 
     if (isUpdated) {
@@ -153,3 +166,68 @@ void checkToggleAutomaticStatus() {
 BLYNK_WRITE(V0) {
     isAutomatic = param.asInt();
 }
+
+BLYNK_WRITE(V1) {
+    int newValue = param.asInt();
+    if (newValue != cooler) {
+        cooler = newValue;
+        isUpdated = true;
+    }
+}
+
+BLYNK_WRITE(V2) {
+    int newValue = param.asInt();
+    if (newValue != heater) {
+        heater = newValue;
+        isUpdated = true;
+    }
+}
+
+BLYNK_WRITE(V3) {
+    int newValue = param.asInt();
+    if (newValue != tdsPlants) {
+        tdsPlants = newValue;
+        isUpdated = true;
+    }
+}
+
+BLYNK_WRITE(V4) {
+    int newValue = param.asInt();
+    if (newValue != tdsFishes) {
+        tdsFishes = newValue;
+        isUpdated = true;
+    }
+}
+
+BLYNK_WRITE(V5) {
+    int newValue = param.asInt();
+    if (newValue != pumpIn) {
+        pumpIn = newValue;
+        isUpdated = true;
+    }
+}
+
+BLYNK_WRITE(V6) {
+    int newValue = param.asInt();
+    if (newValue != pumpOut) {
+        pumpOut = newValue;
+        isUpdated = true;
+    }
+}
+
+BLYNK_WRITE(V7) {
+    int newValue = param.asInt();
+    if (newValue != ventilation) {
+        ventilation = newValue;
+        isUpdated = true;
+    }
+}
+
+BLYNK_WRITE(V8) {
+    int newValue = param.asInt();
+    if (newValue != mistSprayer) {
+        mistSprayer = newValue;
+        isUpdated = true;
+    }
+}
+
